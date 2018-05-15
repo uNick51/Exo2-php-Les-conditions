97482af82dd82318f352af0ac92b0908b4cc0183
uNick51 <nick51@hotmail.fr>
2018-05-15 20:38:40 +0200

    fait

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
   <?php
 
-  // -----------Exercice1------------
+
   $age = 38;
   if ($age >= 18) {
       echo "Vous êtes majeur.";
@@ -22,38 +22,38 @@
 
   $age = ($age >= 18) ? majeur : mineur;
   $genre = ($genre == "homme") ? "un homme" : "une femme";
-  echo "Vous êtes ".$genre." et vous êtes ".$age;
+  echo "<br />"."Vous êtes ".$genre." et vous êtes ".$age;
 
   // // -----------Exercice3------------
   $maVariable = "homme";
   if ($maVariable=="homme") {
-    echo "C\'est un développeur !!!";
+    echo "<br />"."C\'est un développeur !!!";
   }else {
-    echo "C\'est une développeuse !!!";
+    echo "<br />"."C\'est une développeuse !!!";
   }
 
   // // -----------Exercice4------------
   $monAge = 8;
   if ($monAge>=18) {
-    echo "Tu es majeur";
+    echo "<br />"."Tu es majeur";
   }else {
-    echo "Tu n'es pas majeur";
+    echo "<br />"."Tu n'es pas majeur";
   }
 
   // // -----------Exercice5------------
   $maVariable = false;
   if ($maVariable== false) {
-    echo "c'est ok !!";
+    echo "<br />"."c'est ok !!";
   }else {
-    echo "c'est pas bon !!!";
+    echo "<br />"."c'est pas bon !!!";
   }
 
   // // -----------Exercice6------------
   $maVariable = "";
   if ($maVariable== "") {
-    echo "c'est ok !!";
+    echo "<br />"."c'est ok !!";
   }else {
-    echo "c'est pas bon !!!";
+    echo "<br />"."c'est pas bon !!!";
   }
   ?>
 
